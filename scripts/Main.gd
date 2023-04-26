aa71451427b16a07dd755d4fa227a42f68b19234
Jose Marquez <josemarquezmontoro2@gmail.com>
2023-04-26 20:05:55 +0200

    project structure

---
+++ scripts/Main.gd
@@ -0,0 +1,10 @@
+extends Node
+
+func _ready():
+	var btn_start_game = get_node("StartButton")
+	btn_start_game.connect("pressed", self, "_on_button_pressed", [btn_start_game])
+
+func _on_button_pressed(button):
+	match button.name:
+		"StartButton":
+			get_tree().change_scene("res://scenes/Minigames.tscn")
