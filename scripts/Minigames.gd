2fa7f895bf325e9ddee2a5abab660dfc7569717a
Jose Marquez <josemarquezmontoro2@gmail.com>
2023-04-28 18:51:55 +0200

    placeholder menu fixed

---
+++ scripts/Minigames.gd
@@ -0,0 +1,23 @@
+extends Node
+
+func _ready():
+	var hbox_container = get_node("HBoxContainer")
+	var btn_race = hbox_container.get_node("Race")
+	btn_race.connect("pressed", self, "_on_button_pressed", [btn_race])
+
+	var btn_ping_pong = hbox_container.get_node("Ping Pong")
+	btn_ping_pong.connect("pressed", self, "_on_button_pressed", [btn_ping_pong])
+
+	var btn_air_hockey = hbox_container.get_node("Air Hockey")
+	btn_air_hockey.connect("pressed", self, "_on_button_pressed", [btn_air_hockey])
+
+func _on_button_pressed(button):
+	match button.name:
+		"Race":
+			get_tree().change_scene("res://scenes/Race.tscn")
+	match button.name:
+		"Ping Pong":
+			get_tree().change_scene("res://scenes/PingPong.tscn")
+	match button.name:
+		"Air Hockey":
+			get_tree().change_scene("res://scenes/AirHockey.tscn")
