8f91137196df344ae4219f3cd2d17cffac1c5175
Jose Márquez <josemarquezmontoro2@gmail.com>
2023-05-27 19:06:19 +0200

    scene elements  restructured

--- scripts/SlotRace/Gameplay.gd
+++ scripts/SlotRace/Gameplay.gd
@@ -5,15 +5,21 @@ var btn_player2: Button
 var car_player1: Node2D
 var car_player2: Node2D
 
+
 func _ready():
-	btn_player1 = get_node("Player1MarginArea/Player1ControlPanel/Player1Button")
+	# init buttons
+	btn_player1 = get_node("UI/Components/Player1MarginArea/Player1ControlPanel/Player1Button")
 	btn_player1.connect("pressed", self, "_on_button_pressed", [btn_player1])
-	btn_player2 = get_node("Player2MarginArea/Player2ControlPanel/Player2Button")
+	btn_player2 = get_node("UI/Components/Player2MarginArea/Player2ControlPanel/Player2Button")
 	btn_player2.connect("pressed", self, "_on_button_pressed", [btn_player2])
-	car_player1 = get_node("GameWorld/Player1Path2D/Player1PathFollow2D/Player1Car")
-	car_player2 = get_node("GameWorld/Player2Path2D/Player2PathFollow2D/Player2Car")
+	
+	# init alpha2d elements
+	car_player1 = get_node("RaceWorld/Player1Path2D/Player1PathFollow2D/Player1Area2D")
+	car_player2 = get_node("RaceWorld/Player2Path2D/Player2PathFollow2D/Player2Area2D")
+
 
 func _on_button_pressed(button: Button):
+	# Accelerate each car depend of the button pressed
 	var path_follow: PathFollow2D = null
 
 	if button == btn_player1:
@@ -24,6 +30,3 @@ func _on_button_pressed(button: Button):
 		path_follow.offset += 50
 	else:
 		print("PathFollow2D not found for button:", button.name)
-
-
-
