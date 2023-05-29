55b4665351483f60f9019399d774434f0053950a
Jun <zhoujun97099@gmail.com>
2023-05-29 23:59:02 +0200

    Created ball scene as well as its physics script

--- scenes/PingPong/Player1.gd
+++ scenes/PingPong/Player1.gd
@@ -5,9 +5,9 @@ var speed = 400
 func _physics_process(delta):
 	var velocity = Vector2.ZERO
 	if Input.is_action_pressed("ui_up"):
-		velocity.y -= 400
+		velocity.x -= 400
 	elif Input.is_action_pressed("ui_down"):
-		velocity.y += 400
+		velocity.x += 400
 	move_and_slide(velocity)
 	
 	
--- scenes/PingPong/Player2.gd
+++ scenes/PingPong/Player2.gd
@@ -4,10 +4,10 @@ var speed = 400
 
 func _physics_process(delta):
 	var velocity = Vector2.ZERO
-	if Input.is_action_pressed("ui_w"):
-		velocity.y -= 400
-	elif Input.is_action_pressed("ui_s"):
-		velocity.y += 400
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= 400
+	elif Input.is_action_pressed("ui_right"):
+		velocity.x += 400
 	move_and_slide(velocity)
 	
 	
