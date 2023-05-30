34924c5f0b57d9906d4f6a7e3dd9a9f2b3206ac5
Jun <zhoujun97099@gmail.com>
2023-05-30 23:35:42 +0200

    Implemented scoring system and ball bouncing. Pending to change font for the scores

--- scenes/PingPong/Player1.gd
+++ scenes/PingPong/Player1.gd
@@ -5,9 +5,9 @@ var speed = 400
 func _physics_process(delta):
 	var velocity = Vector2.ZERO
 	if Input.is_action_pressed("ui_up"):
-		velocity.x -= 400
+		velocity.x -= 600
 	elif Input.is_action_pressed("ui_down"):
-		velocity.x += 400
+		velocity.x += 600
 	move_and_slide(velocity)
 	
 	
