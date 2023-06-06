1696423aec97dca42889ec3db8f197ff8a7e1cb3
Jun <zhoujun97099@gmail.com>
2023-06-07 01:07:23 +0200

    Restore data

---
+++ scenes/PingPong/Nivel/Level.gd
@@ -0,0 +1,52 @@
+extends Node
+
+var Pj1Score = 0
+var Pj2Score = 0
+const EndScreenScene = preload("res://scenes/PingPong/EndScreen.tscn")
+export var MaxPoint = 11
+
+	
+func _on_TopWall_body_entered(body):
+	Pj2Score += 1
+	pj_score()
+
+	
+func _on_BottonWall_body_entered(body):
+	Pj1Score += 1
+	pj_score()
+	
+
+func pj_score():
+	$Ball.position = Vector2(360, 540)
+	get_tree().call_group('BallGroup', 'stop_ball')
+	$BallTimer.start()
+	$Countdown.visible = true
+	$PointSound.play()
+	$Player1.position.x = 435
+	$Player2.position.x = 724
+	
+	if Pj1Score == MaxPoint or Pj2Score == MaxPoint:
+		if Pj1Score == MaxPoint:
+			GlobalAtributes.winner = str(1)
+		else:
+			GlobalAtributes.winner = str(2)
+		go_to_end_screen()
+		$WinnerSound.play()	
+
+
+func go_to_end_screen():
+	get_tree().change_scene_to(EndScreenScene)
+	
+	
+func _process(delta):    
+	$Pj1Score.text = str(Pj1Score)
+	$Pj2Score.text = str(Pj2Score)
+	$Countdown.text = str(int($BallTimer.time_left) + 1)
+		
+
+func _on_BallTimer_timeout():
+	get_tree().call_group('BallGroup','restart_ball')
+	$Countdown.visible = false
+
+
+
