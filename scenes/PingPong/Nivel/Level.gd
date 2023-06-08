019f53f79073d2c8f8a7f033ab1296f8bcb6b70a
Jun <zhoujun97099@gmail.com>
2023-06-08 14:17:25 +0200

    fixed ball behaviour

--- scenes/PingPong/Nivel/Level.gd
+++ scenes/PingPong/Nivel/Level.gd
@@ -7,32 +7,13 @@ export var MaxPoint = 11
 
 	
 func _on_TopWall_body_entered(body):
-	Pj2Score += 1
 	pj_score()
+	Pj2Score += 1
 
 	
 func _on_BottonWall_body_entered(body):
-	Pj1Score += 1
 	pj_score()
-	
-
-func pj_score():
-	$Ball.position = Vector2(366, 599)
-	get_tree().call_group('BallGroup', 'stop_ball')
-	$BallTimer.start()
-	$Countdown.visible = true
-	$PointSound.play()
-	$Player1.position.x = 438
-	$Player2.position.x = 713
-	
-	if Pj1Score == MaxPoint or Pj2Score == MaxPoint:
-		if Pj1Score == MaxPoint:
-			GlobalAttributes.winner = str(1)
-		else:
-			GlobalAttributes.winner = str(2)
-			
-		go_to_end_screen()
-	
+	Pj1Score += 1
 
 
 func go_to_end_screen():
@@ -44,10 +25,28 @@ func _process(delta):
 	$Pj2Score.text = str(Pj2Score)
 	$Countdown.text = str(int($BallTimer.time_left) + 1)
 		
+func pj_score():
+	$Ball.position = Vector2(366, 599)
+	get_tree().call_group('BallGroup', 'stop_ball')
+	$BallTimer.start()
+	$Countdown.visible = true
+	$PointSound.play()
+	$Player1.position.x = 438
+	$Player2.position.x = 713
+
+	if Pj1Score == MaxPoint or Pj2Score == MaxPoint:
+		if Pj1Score == MaxPoint:
+			GlobalAttributes.winner = str(1)
+		else:
+			GlobalAttributes.winner = str(2)
+			
+		go_to_end_screen()
+	
 
 func _on_BallTimer_timeout():
 	get_tree().call_group('BallGroup','restart_ball')
 	$Countdown.visible = false
+
 
 
 
