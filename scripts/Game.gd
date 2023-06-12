bf4c61f0da42ee99d2c372c17a2a6a99f9e7b2dc
Pedrojimvi <pedrojv1030@gmail.com>
2023-06-12 18:56:05 +0200

    New Changes
    New Changes

--- scripts/Game.gd
+++ scripts/Game.gd
@@ -22,37 +22,23 @@ var p2_target_position
 var blue_score = 0
 var red_score = 0
 
-var vibration_supported = false
-
 onready var blue = $Blue
 onready var red = $Red
-onready var blue_goal_particles = $BlueGoalParticles
-onready var red_goal_particles = $RedGoalParticles
 
 onready var puck = $Puck
 onready var puck_sprite = $Puck/PuckSprite
-onready var puck_explosion_particles = $PuckExplosionParticles
 onready var puck_timer = $PuckTimer
-
-onready var tween = $Tween
 
 onready var spawn_sound = $SpawnSound
 onready var score_sound = $ScoreSound
 
 func _ready():
 	spawn_sound.play()
-	blue.position = Vector2(100, 300)
-	red.position = Vector2(924, 300)
+	blue.position = Vector2(75, 300)
+	red.position = Vector2(945, 300)
 	puck.position = Vector2(512, 300)
 	puck.linear_velocity = Vector2.ZERO
 	puck.angular_velocity = 0
-	tween.interpolate_property(puck_sprite, "modulate", \
-			Color(1, 1, 1, 0), Color(1, 1, 1, 1), 1, Tween.TRANS_CUBIC)
-	tween.interpolate_property(puck_sprite, "scale", \
-			Vector2(1.5, 1.5), Vector2(1, 1), 1, Tween.TRANS_CUBIC)
-	tween.start()
-	if OS.get_name() == "Android" || "iOS":
-		vibration_supported = true
 
 func _unhandled_input(event):
 	if event is InputEventScreenDrag:
@@ -80,52 +66,31 @@ func _on_PuckVisibilityNotifier_screen_exited():
 func _on_BlueGoal_body_entered(body):
 	if body.name == "Puck" and score_allowed:
 		goal()
-		blue_goal_particles.emitting = true
 		emit_signal("red_scored")
 
 func _on_RedGoal_body_entered(body):
 	if body.name == "Puck" and score_allowed:
 		goal()
-		red_goal_particles.emitting = true
 		emit_signal("blue_scored")
 
 func goal():
 	puck.hide()
 	puck.set_deferred("mode", RigidBody2D.MODE_STATIC)
-	puck_explosion_particles.position = puck.position
-	puck_explosion_particles.direction = puck.linear_velocity
-	puck_explosion_particles.emitting = true
 	score_allowed = false
 	auto_centering = false
 	score_sound.play()
-	if vibration_supported:
-		Input.vibrate_handheld()
 	puck_timer.start()
 
-func _on_Tween_tween_step(_object, _key, elapsed, _value):
-	if elapsed >= 0.5:
-		puck.mode = RigidBody2D.MODE_RIGID
 
 func _on_PuckTimer_timeout():
 	center_puck()
+	puck.set_deferred("mode", RigidBody2D.MODE_RIGID)
 	puck.show()
 	score_allowed = true
 	auto_centering = true
 
 func center_puck():
 	spawn_sound.play()
-	tween.interpolate_property(puck_sprite, "modulate", \
-				Color(1, 1, 1, 0), Color(1, 1, 1, 1), 1, Tween.TRANS_CUBIC)
-	tween.interpolate_property(puck_sprite, "scale", \
-				Vector2(1.5, 1.5), Vector2(1, 1), 1, Tween.TRANS_CUBIC)
-	tween.start()
 	puck.position = Vector2(512, 300)
 	puck.linear_velocity = Vector2.ZERO
 	puck.angular_velocity = 0
-
-func _on_AttackTimer_timeout():
-	if auto_centering:
-		if randf() > 0.5:
-			red_attack = true
-		if randf() > 0.5:
-			blue_attack = true
--- scripts/HUD.gd
+++ scripts/HUD.gd
@@ -10,13 +10,6 @@ var game_finished = false
 onready var table = $Table
 onready var blue_score_label = $BlueScore
 onready var red_score_label = $RedScore
-onready var blue_wins_overlay = $BlueWinsOverlay
-onready var red_wins_overlay = $RedWinsOverlay
-onready var blue_scores = $TextCenter/BlueScores
-onready var red_scores = $TextCenter/RedScores
-
-onready var tween = $Tween
-onready var scoring_animation = $TextCenter/ScoringAnimation
 
 func _unhandled_input(event):
 	if event is InputEventScreenTouch and game_finished == true:
@@ -24,8 +17,6 @@ func _unhandled_input(event):
 			new_game()
 
 func new_game():
-	blue_wins_overlay.visible = false
-	red_wins_overlay.visible = false
 	table._ready()
 	game_finished = false
 	blue_score = 0
@@ -37,29 +28,10 @@ func _on_blue_scored():
 	blue_score += 1
 	blue_score_label.text = "Blue : " + str(blue_score)
 	if blue_score == target_score and game_finished == false:
-		tween.interpolate_property(blue_wins_overlay, "modulate", \
-				Color(1, 1, 1, 0), Color(1, 1, 1, 1), 0.5, Tween.TRANS_CUBIC)
-		tween.start()
-		blue_wins_overlay.visible = true
 		game_finished = true
-	else:
-		blue_scores.visible = true
-		scoring_animation.play("PlayerScore")
 
 func _on_red_scored():
 	red_score += 1
 	red_score_label.text = "Red : " + str(red_score)
 	if red_score == target_score and game_finished == false:
-		tween.interpolate_property(red_wins_overlay, "modulate", \
-				Color(1, 1, 1, 0), Color(1, 1, 1, 1), 0.5, Tween.TRANS_CUBIC)
-		tween.start()
-		red_wins_overlay.visible = true
 		game_finished = true
-	else:
-		red_scores.visible = true
-		scoring_animation.play("PlayerScore")
-
-func _on_ScoringAnimation_animation_finished(anim_name):
-	if anim_name == "PlayerScore":
-		blue_scores.hide()
-		red_scores.hide()
